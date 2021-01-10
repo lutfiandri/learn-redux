67810c574b0d51338c494551159fe19c3984d751
lutfiandri <lutfiandri232@gmail.com>
2021-01-10 23:18:33 +0700

    basic redux

---
+++ src/components/BuyCake.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
+
+const BuyCake = ({numOfCakes, buyCake}) => {
+  return (
+    <div>
+      <h1>number of cakes --- {numOfCakes}</h1>
+      <button onClick={buyCake}>buy cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    // masuk ke props
+    numOfCakes: state.numOfCakes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // masuk ke props
+    buyCake: () => dispatch(buyCake())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BuyCake);
